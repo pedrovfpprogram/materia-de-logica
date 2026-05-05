99044661d8b19ccd734e995fa136f251aa4ecdff
Pedro <pedrovfpprogram@yahoo.com>
2026-05-05 10:54:20 +0000

    while e for

---
+++ repeticao/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int contador = 0;
+
+    for(int i = 0; i < 5; i++) {
+        cout << "Valor: " << i << endl;
+    }
+/*
+    while (contador<5) {
+        cout << "Numero: " << contador << endl;
+        contador++;
+    }
+*/
+
+
+    return 0;
+}
